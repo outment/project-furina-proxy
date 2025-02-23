19b749a602f144885f4fa368ceb5440d09a8172d
Mango <144482551+outment@users.noreply.github.com>
2025-02-22 22:20:18 -0800

    Testing new fix.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http')
 const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer({})
 
+const PORT = process.env.PORT || 3000
+
 http.createServer((req, res) => {
   const target = 'http://147.185.221.26:12086' + req.url
   proxy.web(req, res, { target })
-}).listen(3000, () => console.log('Project Furina Reverse Proxy is now active.'))
+}).listen(PORT, () => console.log(`Project Furina Reverse Proxy is now active on port ${PORT}.`))
