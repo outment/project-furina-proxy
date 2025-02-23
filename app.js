d1aefb230cdcc734980975dd6bd3951ad35f7d04
Mango <144482551+outment@users.noreply.github.com>
2025-02-22 22:14:31 -0800

    Removing unnecessery stuff + using HTTP-Proxy.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const http = require('http')
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer({})
 
+http.createServer((req, res) => {
+  const target = 'http://147.185.221.26:12086' + req.url
+  proxy.web(req, res, { target })
+}).listen(3000, () => console.log('Project Furina Reverse Proxy is now active.'))
